f147cb130fe95e10749153a1090ecba6d579e1f5
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-09-09 03:45:50 +0300

    Created navBar items lists.

---
+++ public/statics/navBarNavigationLists.ts
@@ -0,0 +1,32 @@
+import { signOut } from 'next-auth/react'
+interface navigationItem {
+  name: string
+  href: string
+  current: boolean
+}
+interface profileDropDownItem {
+  label: string
+  href?: string
+  functionality?: () => void
+  type: 'link' | 'button'
+}
+
+const doSignOut = () => {
+  signOut({
+    redirect: true,
+    callbackUrl: '/'
+  })
+}
+
+const navigationList: navigationItem[] = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false }
+]
+
+const profileDropdownList: profileDropDownItem[] = [
+  { label: 'Your Profile', href: '#', type: 'link' },
+  { label: 'Settings', href: '#', type: 'link' },
+  { label: 'Sign out', type: 'button', functionality: () => doSignOut() }
+]
+export { navigationList, profileDropdownList }
