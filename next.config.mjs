520c2782916e4889adf8ed384009f5d0c29c12cf
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-09-09 03:45:05 +0300

    Added remotePatterns array to provide anomalous hosts fetching.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // Specify the protocol
+        hostname: 'avatars.githubusercontent.com', // Specify the hostname
+        port: '', // Leave empty if no specific port is needed
+        pathname: '/u/**' // Match paths starting with /u/, using wildcard for any number of segments
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
+
